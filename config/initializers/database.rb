05ee827f2d62726ceb018560f2f9206ab691d04f
RamilGilmanov <ramil.gilmanov@flatstack.com>
2015-08-24 19:24:17 +0300

    Remove default options

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -1,2 +1,2 @@
 db_config = File.read(File.expand_path("../../bot_database.yml", __FILE__))
-DB_CONFIG = YAML.load(ERB.new(db_config).result) rescue {}
+DB_CONFIG = YAML.load(ERB.new(db_config).result)
