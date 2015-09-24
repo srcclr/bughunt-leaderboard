654b137de2b434b87e1d760517989459c8d17b0b
RamilGilmanov <ramil.gilmanov@flatstack.com>
2015-09-24 18:51:50 +0300

    Add margin right after the board

--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,12 @@
 # name: leaderboard
 # about: BugHunt Leaderboard
-# version: 0.0.1
-
-# register_asset("javascripts/discourse/templates/leaderboard/boards.hbs")
-# register_asset("javascripts/discourse/leaderboard-route-map.js.es6")
-# register_asset("stylesheets/leaderboard/application.css.scss")
+# version: 0.0.2
 
 gem "mysql2", "0.3.19"
 
 require(File.expand_path("../lib/leaderboard", __FILE__))
+
+register_asset("stylesheets/views/leaderboard.css.scss")
 
 Discourse::Application.routes.append do
   mount Leaderboard::Engine, at: "/bughunt"
