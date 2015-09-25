d8cac40f34e5040c3d9f3891c33698160a11bded
RamilGilmanov <ramil.gilmanov@flatstack.com>
2015-09-25 12:52:44 +0300

    Remove excessive css

--- plugin.rb
+++ plugin.rb
@@ -6,8 +6,6 @@ gem "mysql2", "0.3.19"
 
 require(File.expand_path("../lib/leaderboard", __FILE__))
 
-register_asset("stylesheets/views/leaderboard.css.scss")
-
 Discourse::Application.routes.append do
   mount Leaderboard::Engine, at: "/bughunt"
 end
